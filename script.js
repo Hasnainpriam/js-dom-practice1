befdeefcc433c327f4598c5c1994085824a5f453
unknown <hasnainpriam925@gmail.com>
2022-01-29 22:53:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+document.getElementById('submit-button').addEventListener('click',function(){
+  const parent=document.getElementById('list-section');
+  const newElement= document.createElement('li');
+  newElement.innerText="Brand New Button";
+  newElement.classList.add('listi');
+  parent.appendChild(newElement);
+ })
+
+const titles = document.getElementsByTagName("h3");
+for (const title of titles) {
+  title.style.color = "red";
+}
+const topTags=document.getElementsByClassName("top-tags");
+for(const topTag of topTags){
+  topTag.style.backgroundColor ="rgba(255, 99, 71, 0.6)";
+}
+
+// const lists=document.getElementsByClassName('listi');
+// for(const list of lists){
+//   list.addEventListener('click',function(event){
+//     event.target.parentNode.removeChild(event.target);
+//   })
+// }
+
+// const lists=document.getElementsByClassName("listi");
+// for(const list of lists){
+//  list.addEventListener('click',function(event){
+//   event.target.parentNode.removeChild(event.target);
+// })
+// }
+
+document.getElementById('list-section').addEventListener('click',function(event){
+  event.target.parentNode.removeChild(event.target);
+})
+
+document.getElementById('plus-button').addEventListener('click',function(){
+  const inputField=document.getElementById('input-field');
+  const inputFieldText=inputField.value;
+  const inputFieldNumber=parseInt(inputFieldText);
+  newValue= inputFieldNumber + 1;
+  inputField.value=newValue;
+  const button= document.getElementById('plus-button');
+  if(newValue<5){
+   button.removeAttribute('disabled');
+  }
+  else{
+    button.setAttribute('disabled',true);
+  }
+})
